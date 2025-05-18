c1c1b0329f0df6cf7da26d7cf7c2991b4e3c3fde
Darveht <168034103+Darveht@users.noreply.github.com>
2025-05-18 02:22:53 -0500

    Update script.js

--- script.js
+++ script.js
@@ -458,7 +458,7 @@ const apps = [{
   "security": true,
   "version": "8.104.0",
   "isAvailable": true,
-  "releaseDate": "2025-05-28T00:00:00",
+  "releaseDate": "2025-05-11T00:00:00",
   "allowedCountries": ["US", "CA", "GB", "AU", "NZ", "MX", "BR", "IN", "JP", "FR", "DE", "ES", "IT", "KR"],
   "platforms": {
     "android": "https://play.google.com/store/apps/details?id=com.netflix.mediaclient",
